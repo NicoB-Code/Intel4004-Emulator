18329d1a10468ab0224a4e780a91011d8f847344
NicoB-Code <bayernico@web.de>
2021-07-11 19:38:16 +0200

    4002 funktionstüchtig

--- 4002.cpp
+++ 4002.cpp
@@ -1,17 +1,184 @@
+// Include local header files
 #include "4002.h"
 
+// Include gloabl header files
+#include <cstring>
+
+// Declaring namespaces
 using namespace std;
 
+Intel4002::Intel4002(const uint32_t installedChips) : currentBank(BANK0), currentChip(CHIP0), currentNibbleAddress(0), RAM(nullptr), RAMStatus(nullptr), RAMPort(nullptr), INSTALLEDRAM(nullptr) {
+    // Initialize RAM, RAMStatus, RAMPort, INSTALLEDRAM
+    RAM = new uint4_t***[MAX_NUMBER_OF_BANKS]();
+    RAMStatus = new uint4_t***[MAX_NUMBER_OF_BANKS]();
+    RAMPort = new uint4_t*[MAX_NUMBER_OF_BANKS]();
+    INSTALLEDRAM = new bool*[MAX_NUMBER_OF_BANKS]();
+    uint32_t chips = installedChips;
 
-Intel4002::Intel4002()
-Intel4002::~Intel4002()
+    for (int i = 0; i < MAX_NUMBER_OF_BANKS; i++) {
+        RAM[i] = new uint4_t**[MAX_NUMBER_OF_RAM_CHIPS]();
+        RAMStatus[i] = new uint4_t**[MAX_NUMBER_OF_RAM_CHIPS]();
+        RAMPort[i] = new uint4_t[MAX_NUMBER_OF_RAM_CHIPS];
+        INSTALLEDRAM[i] = new bool[MAX_NUMBER_OF_RAM_CHIPS];
 
-Intel4002::bool isRAMAdrAccessable(const ERAMBank bank, const ERAMChip chips) const
-Intel4002::uint4_t readRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const
-Intel4002::bool writeRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value)
-Intel4002::bool isStatusAdrAccessable(const ERAMBank bank, const ERAMChip chip) const
-Intel4002::uint4_t readStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const
-Intel4002::bool writeStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value)
-Intel4002::uint4_t readFromPortBuffer(const ERAMBank bank, const ERAMChip chip) const
-Intel4002::ERAMCommand nextCommand(Intel4004Base *ptr, const UCommand command);
+        for (int j = 0; j < MAX_NUMBER_OF_RAM_CHIPS; j++, chips >>= 1) {
+            RAM[i][j] = new uint4_t*[MAX_NUMBER_OF_RAM_REGISTERS];
+            RAMStatus[i][j] = new uint4_t*[MAX_NUMBER_OF_RAM_REGISTERS];
 
+            INSTALLEDRAM[i][j] = chips & 1;
+
+            for (int k = 0; k < MAX_NUMBER_OF_RAM_REGISTERS; k++) {
+                RAM[i][j][k] = new uint4_t[RAM_CELLS_EACH_REGISTER];
+                RAMStatus[i][j][k] = new uint4_t[STATUS_CELLS_EACH_REGISTER];
+            }
+        }
+    }
+
+    reset();
+}
+
+Intel4002::~Intel4002() {
+    //  RAM, RAMSTATUS, RAMPORT, INSTALLEDRAM FREIGEBEN
+    for (int i = 0; i < MAX_NUMBER_OF_BANKS; i ++) {
+        for (int j = 0; j < MAX_NUMBER_OF_RAM_CHIPS; j++) {
+            for (int k = 0; k < MAX_NUMBER_OF_RAM_REGISTERS; k++) {
+                delete[] RAM[i][j][k];
+                delete[] RAMStatus[i][j][k];
+            }
+            delete[] RAM[i][j];
+            delete[] RAMStatus[i][j];
+        }
+        delete[] RAM[i];
+        delete[] RAMStatus[i];
+        delete[] RAMPort[i];
+        delete[] INSTALLEDRAM[i];
+    }
+    delete[] RAM;
+    delete[] RAMStatus;
+    delete[] RAMPort;
+    delete[] INSTALLEDRAM;
+}
+
+void Intel4002::reset() {
+    currentBank = BANK0;
+    currentChip = CHIP0;
+    currentRegister = REG0;
+    currentNibbleAddress = 0;
+
+    for (int i = 0; i < MAX_NUMBER_OF_BANKS; i++) {
+        for (int j = 0; j < MAX_NUMBER_OF_RAM_CHIPS; j++) {
+            for (int k = 0; k < MAX_NUMBER_OF_RAM_REGISTERS; k++) {
+                memset(RAM[i][j][k], 0, RAM_CELLS_EACH_REGISTER);
+                memset(RAMStatus[i][j][k], 0, STATUS_CELLS_EACH_REGISTER);
+            }
+        }
+        memset(RAMPort[i], 0, MAX_NUMBER_OF_RAM_CHIPS);
+    }
+}
+
+bool Intel4002::isRAMAdrAccessable(const ERAMBank bank, const ERAMChip chip) const {
+    return INSTALLEDRAM[bank][chip];
+}
+
+uint4_t Intel4002::readRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const {
+    if (isRAMAdrAccessable(bank, chip)) {
+        return RAM[bank][chip][ramregister][(nibbleaddress & 0b1111)];
+    }
+    return 0;
+}
+
+bool Intel4002::writeRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value) {
+    if (isRAMAdrAccessable(bank, chip)) {
+        RAM[bank][chip][ramregister][(nibbleaddress & 0b1111)] = value;
+        return true;
+    }
+    return false;
+}
+
+bool Intel4002::isStatusAdrAccessable(const ERAMBank bank, const ERAMChip chip) const {
+    return INSTALLEDRAM[bank][chip];
+}
+
+uint4_t Intel4002::readStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const {
+    if (isStatusAdrAccessable(bank, chip)) {
+        return RAMStatus[bank][chip][ramregister][(nibbleaddress & 0b11)];
+    }
+    return 0;
+}
+
+bool Intel4002::writeStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value) {
+    if (isStatusAdrAccessable(bank, chip)) {
+        RAMStatus[bank][chip][ramregister][(nibbleaddress & 0b11)] = value;
+        return true;
+    }
+    return false;
+}
+
+uint4_t Intel4002::readFromPortBuffer(const ERAMBank bank, const ERAMChip chip) const {
+    if (isRAMAdrAccessable(bank, chip)) {
+        return RAMPort[bank][chip];
+    }
+    return 0;
+}
+
+// Eigene Funktionen
+
+uint4_t Intel4002::readRAM() {
+    return readRAMNibble(currentBank, currentChip, currentRegister, currentNibbleAddress);
+}
+
+bool Intel4002::writeRAM(const uint4_t value) {
+    return writeRAMNibble(currentBank, currentChip, currentRegister, currentNibbleAddress, value);
+}
+
+uint4_t Intel4002::readStatus(const int address) {
+    return readStatusNibble(currentBank, currentChip, currentRegister, (address & 0b11));
+}
+
+bool Intel4002::writeStatus(const int address, const uint4_t value) {
+    return writeStatusNibble(currentBank, currentChip, currentRegister, (address & 0b11), value);
+}
+
+uint4_t Intel4002::readPortBurffer() {
+    return readFromPortBuffer(currentBank, currentChip);
+}
+
+bool Intel4002::writePortBuffer(uint4_t value) {
+    if (isRAMAdrAccessable(currentBank, currentChip)) {
+        RAMPort[currentBank][currentChip] = value;
+        return true;
+    }
+    return false;
+}
+
+void Intel4002::setCurrentBank(const ERAMBank bank) {
+    if (BANK0 <= bank && bank <= BANK7) {
+        currentBank = bank;
+    }
+}
+
+// ERAMBank Intel4002::getCurrentBank() const {
+//     return currentBank;
+// }
+
+void Intel4002::setCurrentChip(const ERAMChip chip) {
+    if (CHIP0 <= chip && chip <= CHIP3) {
+        currentChip = chip;
+    }
+}
+
+// ERAMChip Intel4002::getCurrentChip() const {
+//     return currentChip;
+// }
+
+void Intel4002::setCurrentRegister(const ERAMRegister reg) {
+    currentRegister = reg;
+}
+
+void Intel4002::setCurrentNibbleAddress(const int address) {
+    currentNibbleAddress = (address & 0b1111);
+}
+
+// int Intel4002::getCurrentAddress() const {
+//     return currentAddress;
+// }
--- 4002.h
+++ 4002.h
@@ -1,27 +1,181 @@
 #ifndef _4002_h_
 #define _4002_h_
 
-#include "MCS4BaseClasses.h"
-#include <iostream>
+// Include local header files
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	#include "..\inc\MCS4BaseClasses.h"
+#elif __unix__
+	#include "../inc/MCS4BaseClasses.h"
+#endif
+
+// Include global header files
+#include <cstdint>
+
+// Declaring namespaces
 using namespace std;
 
-/** Basisklasse Intel 4002 RAM.*/
 class Intel4002 : public Intel4002Base {
 public:
-	Intel4002() { 
-	}
-	~Intel4002() {
-	}
- void reset();
-	bool isRAMAdrAccessable(const ERAMBank bank, const ERAMChip chips) const;
-	uint4_t readRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const;
-	bool writeRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value);
-	bool isStatusAdrAccessable(const ERAMBank bank, const ERAMChip chip) const;
-	uint4_t readStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const;
-	bool writeStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value);
-	uint4_t readFromPortBuffer(const ERAMBank bank, const ERAMChip chip) const;
+	enum {
+        RAM_CELLS_EACH_REGISTER = 16,
+		STATUS_CELLS_EACH_REGISTER = 4,
+		MAX_NUMBER_OF_RAM_REGISTERS = 4,
+        MAX_NUMBER_OF_RAM_CHIPS = 4,
+		MAX_NUMBER_OF_BANKS = 8
+    };
+	/**
+	 * Konstruktor
+	 */
+	Intel4002(const uint32_t installedChips = 0xFFFFFFFF);
+	/**
+	 * Destruktor
+	 */
+	virtual ~Intel4002();
+	/**
+	 * Setzt die internen Register zurück zurück
+	 */
+	virtual void reset();
+	/**
+	 * Ist die RAM Adresse vorhanden.
+	 * Falls nicht wird false zurückgegeben
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return <c>true</c> wenn addressierbar, sonst <c>false</c>
+	 */
+	virtual bool isRAMAdrAccessable(const ERAMBank bank, const ERAMChip chip) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return Wert
+	 */
+	virtual uint4_t readRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @param value Wert
+	 * @return Ob an dieser Stelle schreibbar war
+	 */
+	virtual bool writeRAMNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value);
+	/**
+	 * Ist die Status Adresse vorhanden.
+	 * Falls nicht wird false zurückgegeben
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return <c>true</c> wenn addressierbar, sonst <c>false</c>
+	 */
+	virtual bool isStatusAdrAccessable(const ERAMBank bank, const ERAMChip chip) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return Wert
+	 */
+	virtual uint4_t readStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @param value Wert
+	 * @return Ob an dieser Stelle schreibbar war
+	 */
+	virtual bool writeStatusNibble(const ERAMBank bank, const ERAMChip chip, const ERAMRegister ramregister, const int nibbleaddress, const uint4_t value);
+	/**
+	 * Liest ein Nibble aus dem Ausgangspuffer
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @return Wert
+	 */
+	virtual uint4_t readFromPortBuffer(const ERAMBank bank, const ERAMChip chip) const;
+	
+	// Unsere besseren Funktionen
 
-#ifdef _RAM_SPLITOFF_
-	 ERAMCommand nextCommand(Intel4004Base *ptr, const UCommand command)
-#endif
+	/**
+	 * Liest ein Nibble an der aktuellen Adresse
+	 * @return Wert
+	 */
+	virtual uint4_t readRAM();
+	/**
+	 * Schreibt ein Nibble an die aktuelle Adresse
+	 * @param value Wert
+	 * @return Ob das Schreiben erfolgreich war
+	 */
+	virtual bool writeRAM(const uint4_t value);
+	/**
+	 * Liest ein Status Nibble an der aktuellen Adresse
+	 * @param address Statusadresse
+	 * @return Wert
+	 */
+	virtual uint4_t readStatus(const int address);
+	/**
+	 * Schreibt ein Status Nibble an die aktuelle Adresse
+	 * @param address Statusadresse
+	 * @param value Wert
+	 * @return Ob an der Stelle schreibbar war
+	 */
+	virtual bool writeStatus(const int address, const uint4_t value);
+	/**
+	 * Liest den Port des aktuell ausgweählten Chips
+	 * @return Wert
+	 */
+	virtual uint4_t readPortBurffer();
+	/**
+	 * Schreibt ein Nibble in den Ausgangspuffer des ausgewählten Chips
+	 * @param value Wert
+	 * @return Ob an dieser Stelle schreibbar war
+	 */
+	virtual bool writePortBuffer(uint4_t value);
+	/**
+	 * Setzt die aktuelle Bank
+	 * @param bank Bank
+	 */
+	virtual void setCurrentBank(const ERAMBank bank);
+		/**
+		 * Gibt die aktuelle Bank zurück
+		 * @return currentBank
+		 */
+		//virtual ERAMBank getCurrentBank() const;
+	/**
+	 * Setzt den aktuellen Chip
+	 * @param chip Chip
+	 */
+	virtual void setCurrentChip(const ERAMChip chip);
+		/**
+		 * Gibt den aktuellen Chip zurück
+		 * @return currentChip
+		 */
+		//virtual ERAMChip getCurrentChip() const;
+	/**
+	 * Setzt das aktuelle Register
+	 * @param reg Register
+	 */
+	virtual void setCurrentRegister(const ERAMRegister reg);
+	/**
+	 * Setzt die aktuelle Adresse
+	 * @param address Adresse
+	 */
+	virtual void setCurrentNibbleAddress(const int address);
+		/**
+		 * Gibt die aktuelle Adresse zurück
+		 * @return currentAddress
+		 */
+		//virtual int getCurrentAddress() const;
+
+private:
+	ERAMBank currentBank;
+	ERAMChip currentChip;
+	ERAMRegister currentRegister;
+	int currentNibbleAddress;
+	uint4_t ****RAM;
+	uint4_t ****RAMStatus;
+	uint4_t **RAMPort;
+	bool **INSTALLEDRAM;
 };
+#endif // _4002_h_
